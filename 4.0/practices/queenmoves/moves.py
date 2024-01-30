a4408a399c3d17a17355cf315baf4507a2f018f1
faithwest <faithchepkoech425@gmail.com>
2024-01-30 23:24:02 +0300

    algorithm for queen moves

--- 4.0/practices/queenmoves/moves.py
+++ 4.0/practices/queenmoves/moves.py
@@ -15,11 +15,30 @@ def is_attack(i, j):
         ##if there is then it reurns true,indicating an attack
         if board[i][k] == 1 or board[k][j] == 1:
             return True
-            
+
     # checking diagonals
+    # checking if there is a queen in the same row or column#
+    #a loop that iterates over the range from 0 to N1.
+        ##ALSO CHECKS each cell ,i, and column ,j, for a queen presence
     for k in range(0, N):
+        ##
         for l in range(0, N):
             if (k + l == i + j) or (k - l == i - j):
                 if board[k][l] == 1:
                     return True
+    return False
+def N_queen(n):
+    # if n is 0, a solution is found
+    if n == 0:
+        return True
+    for i in range(0, N):
+        for j in range(0, N):
+            # checking if we can place a queen here or not
+            # queen will not be placed if the place is being attacked or already occupied
+            if (not is_attack(i, j)) and (board[i][j] != 1):
+                board[i][j] = 1
+                # recursion: whether we can put the next queen with this arrangement or not
+                if N_queen(n - 1) == True:
+                    return True
+                board[i][j] = 0
     return False
