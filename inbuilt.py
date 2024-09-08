f51fc514564d6f22cefe39f5015d5b1b1052ab69
faithwest <faithchepkoech425@gmail.com>
2024-09-08 10:08:45 +0300

    inbuilt functions

---
+++ inbuilt.py
@@ -0,0 +1,11 @@
+text = "Hello"
+print(len(text))  # 5
+print(type(text))  # <class 'str'>
+print(int(3.14))  # 3
+
+"""Common Built-in Functions
+Python provides several built-in functions for everyday tasks, such as:
+
+len(): Returns the length of an object.
+type(): Returns the type of an object.
+int(), float(), str(): Converts between data types."""
